2463f517c824db89d77aaa4c5f7ac8ccf88af0f6
megumin616 <toncom616@gmail.com>
2024-06-27 14:41:50 +0700

    Add project Laravel 11 CRUD

---
+++ app/Models/Student.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sid',
+        'fname',
+        'lname',
+        'std_prg_id',
+    ];
+
+    public function program()
+    {
+        return $this->belongsTo(Program::class, 'std_prg_id');
+    }
+    //ความสัมพันธ์: Student หนึ่งลงทะเบียนใน Program หนึ่ง (Many-to-One) 
+    //เหตุผล: นักศึกษาหนึ่งคนต้องลงทะเบียนเรียนในหลักสูตรหนึ่ง
+
+    public function vaccineRecords()
+    {
+        return $this->hasMany(VaccineRecord::class, 'std_id');
+    }
+    //ความสัมพันธ์: Student หนึ่งมี VaccineRecord หลายตัว (One-to-Many)
+    //เหตุผล: นักศึกษาหนึ่งคนสามารถมีประวัติการฉีดวัคซีนหลายครั้ง
+}
